3e0383aff3b66ec3f6c2bcc1a030d2a6124d398e
Haxords <159933871+Haxords@users.noreply.github.com>
2026-08-12 15:49:04 +0600

    Update MainActivity.kt

--- app/src/main/java/com/greenlaboratories/fieldops/MainActivity.kt
+++ app/src/main/java/com/greenlaboratories/fieldops/MainActivity.kt
@@ -1,23 +1,12 @@
 package com.greenlaboratories.fieldops
 
 import android.os.Bundle
-import android.widget.TextView
-import android.view.Gravity
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        val textView = TextView(this).apply {
-            text = "Green Laboratories Field Ops\n\nApp Active & Working!"
-            textSize = 22f
-            setTextColor(Color.BLACK)
-            gravity = Gravity.CENTER
-        }
-        
-        setContentView(textView)
+        setContentView(R.layout.activity_main)
     }
 }
 
